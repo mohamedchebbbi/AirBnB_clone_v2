189ed0ee1643d0b539dd15524d036e43067f18b7
mohamed chebbi <mohamedchebbbi@gmail.com>
2023-09-07 17:51:38 -0700

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,53 +4,34 @@ Fabric script based on file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import local, put, run, env
-from datetime import datetime
 
-env.user = 'ubuntu'
+from fabric.api import local
+from fabric.api import get
+from fabric.api import put
+from fabric.api import reboot
+from fabric.api import run
+from fabric.api import sudo
+from fabric.api import env
 env.hosts = ['54.208.28.27', '52.91.101.127']
 
 
-def do_pack():
-    """
-    Targging project directory into a packages as .tgz
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    local('sudo mkdir -p ./versions')
-    path = './versions/web_static_{}'.format(now)
-    local('sudo tar -czvf {}.tgz web_static'.format(path))
-    name = '{}.tgz'.format(path)
-    if name:
-        return name
-    else:
-        return None
-
-
 def do_deploy(archive_path):
-    """Deploy the boxing package tgz file
-    """
+    """ deploy my archive tgz into my servers """
     try:
-        archive = archive_path.split('/')[-1]
-        path = '/data/web_static/releases/' + archive.strip('.tgz')
-        current = '/data/web_static/current'
-        put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
-        run('rm /tmp/{}'.format(archive))
-        run('mv {}/web_static/* {}'.format(path, path))
-        run('rm -rf {}/web_static'.format(path))
-        run('rm -rf {}'.format(current))
-        run('ln -s {} {}'.format(path, current))
-        print('New version deployed!')
+        put(archive_path, '/tmp/')
+        c1 = 'mkdir -p /data/web_static/releases/{}/'
+        run(c1.format(archive_path[9:-4]))
+        c2 = 'tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        run(c2.format(archive_path[9:], archive_path[9:-4]))
+        run('rm /tmp/{}'.format(archive_path[9:]))
+        c3 = 'mv /data/web_static/releases/{}/web_static/* \
+              /data/web_static/releases/{}/'
+        run(c3.format(archive_path[9:-4], archive_path[9:-4]))
+        c4 = 'rm -rf  /data/web_static/releases/{}/web_static/'
+        run(c4.format(archive_path[9:-4]))
+        run('rm -rf /data/web_static/current')
+        c5 = 'ln -s /data/web_static/releases/{}/ {}'
+        run(c5.format(archive_path[9:-4], '/data/web_static/current'))
         return True
     except:
         return False
-
-
-def deploy():
-    """
-    A function to call do_pack and do_deploy
-    """
-    archive_path = do_pack()
-    answer = do_deploy(archive_path)
-    return answer
